4248fddec67ff7339f7c79551bbadd86fbf3750c
alek101 <47416711+alek101@users.noreply.github.com>
2021-01-26 17:12:32 +0100

    migration

---
+++ database/migrations/2021_01_26_155327_create_purchases_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('customer_email');
+            $table->foreignId('product_id')->constrained();
+            // $table->foreign('product_id')->refrences('id')->on('products');
+            $table->integer('amount');
+            $table->decimal('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
